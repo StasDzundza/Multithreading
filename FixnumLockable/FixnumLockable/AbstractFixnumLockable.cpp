20bec0cebdda0e2884ed46f0d7179bf056d6ab46
stas_dzundza <stas.dzundza@gmail.com>
2019-11-18 17:18:44 +0200

    added realization for methods lock,unlock,try_lock

--- FixnumLockable/FixnumLockable/AbstractFixnumLockable.cpp
+++ FixnumLockable/FixnumLockable/AbstractFixnumLockable.cpp
@@ -6,23 +6,27 @@
 namespace thread_sync {
 	thread_local int AbstractFixnumLockable::thread_id = -1;
 
+	AbstractFixnumLockable::AbstractFixnumLockable(){
+		allocator = new allocator::IDAllocator(100);
+	}
+
 	AbstractFixnumLockable::AbstractFixnumLockable(allocator::IDAllocator* allocator) {
 		this->allocator = allocator;
 	}
 
 	unsigned int AbstractFixnumLockable::get_id() {
-		while (lock.test_and_set()) { std::this_thread::yield(); }
+		while (locker.test_and_set()) { std::this_thread::yield(); }
 		if (thread_id <= 0) {
 			thread_id = allocator->alloc(&thread_id);
 		}
-		lock.clear();
+		locker.clear();
 		return thread_id;
 	}
 
 	bool AbstractFixnumLockable::register_thread() {
-		while (lock.test_and_set()) { std::this_thread::yield(); }
+		while (locker.test_and_set()) { std::this_thread::yield(); }
 		thread_id = allocator->alloc(&thread_id);
-		lock.clear();
+		locker.clear();
 		return (thread_id < 0) ? false : true;
 	}
 
@@ -31,14 +35,34 @@ namespace thread_sync {
 	}
 
 	void AbstractFixnumLockable::reset(unsigned int new_max_id) {
-		while (lock.test_and_set()) { std::this_thread::yield(); }
+		while (locker.test_and_set()) { std::this_thread::yield(); }
 		allocator->reset(new_max_id);
-		lock.clear();
+		locker.clear();
 	}
 
 	void AbstractFixnumLockable::reset(unsigned int new_min_id, unsigned  int new_max_id) {
-		while (lock.test_and_set()) { std::this_thread::yield(); }
+		while (locker.test_and_set()) { std::this_thread::yield(); }
 		allocator->reset(new_min_id, new_max_id);
-		lock.clear();
+		locker.clear();
+	}
+	void AbstractFixnumLockable::lock(){
+		get_id();
+	}
+	void AbstractFixnumLockable::unlock(){
+		unregister_thread();
+	}
+	bool AbstractFixnumLockable::try_lock(){
+		int prev_id = thread_id;
+		int new_id = get_id();
+		if (prev_id < 0 && new_id >= 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+	AbstractFixnumLockable::~AbstractFixnumLockable(){
+		delete allocator;
 	}
 }
--- FixnumLockable/FixnumLockable/AbstractFixnumLockable.h
+++ FixnumLockable/FixnumLockable/AbstractFixnumLockable.h
@@ -7,15 +7,20 @@
 namespace thread_sync {
 	class AbstractFixnumLockable : public FixnumLockable {
 	public:
+		AbstractFixnumLockable();
 		AbstractFixnumLockable(allocator::IDAllocator* allocator);
 		unsigned int get_id()override;
 		bool register_thread()override;
 		void unregister_thread()override;
 		void reset(unsigned int new_max_id)override;
 		void reset(unsigned int new_min_id, unsigned int new_max_id)override;
+		void lock()override;
+		void unlock()override;
+		bool try_lock()override;
+		virtual ~AbstractFixnumLockable();
 	private:
 		allocator::IDAllocator* allocator;
 		thread_local static int thread_id;
-		std::atomic_flag lock = ATOMIC_FLAG_INIT;
+		std::atomic_flag locker = ATOMIC_FLAG_INIT;
 	};
 }
