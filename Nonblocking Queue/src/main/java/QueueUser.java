686c34074404cc3071de92f70321aa1737e4a937
StasDzundza <stas.dzundza@gmail.com>
2020-02-03 19:37:34 +0200

    added non-blocking queue

---
+++ Nonblocking Queue/src/main/java/QueueUser.java
@@ -0,0 +1,24 @@
+public class QueueUser implements Runnable{
+    private QueueCAS<Integer>queue;
+    private int times;
+    private UseFunction useFunction;
+
+    public QueueUser(QueueCAS<Integer>queue,int times){
+        this.queue = queue;
+        this.times = times;
+        useFunction = (q)->{
+            for(int i = 0; i < times; i++){
+                q.offer(i);
+            }
+        };
+    }
+
+    public void setUseFunction(UseFunction useFunction){
+        this.useFunction = useFunction;
+    }
+
+    @Override
+    public void run() {
+        useFunction.use(queue);
+    }
+}
